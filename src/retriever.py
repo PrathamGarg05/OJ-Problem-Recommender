4341b5ee664db300935255aa364f55feef746352
Pratham Garg <prathamgarg0445@gmail.com>
2026-07-17 18:26:55 +0530

    fix: filter distances according to filtered results

--- src/retriever.py
+++ src/retriever.py
@@ -7,9 +7,9 @@ def retrieve(query, top_k = TOP_K):
         query_texts=[query],
         n_results=top_k
     )
-    print([d for d in result['distances'][0]])
     filtered_results = [i for i , d in enumerate(result['distances'][0]) if d < SIMILARITY_THRESHOLD and d > EXACT_MATCH_THRESHOLD]
     result['metadatas'][0] = [result['metadatas'][0][i] for i in filtered_results]
     result['documents'][0] = [result['documents'][0][i] for i in filtered_results]
+    result['distances'][0] = [result['distances'][0][i] for i in filtered_results]
     return result
 
